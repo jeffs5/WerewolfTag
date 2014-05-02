c166003d3f4182b4fe4f42966def3a3db5148462
jeffs5 <jeffs5s@gmail.com>
2014-05-01 17:37:41 -0700

    Message sending changes

--- client.py
+++ client.py
@@ -9,6 +9,7 @@ import Player
 import time
 
 players = []
+borders = []
 player = None 
 
 class Client(Handler):
@@ -18,14 +19,20 @@ class Client(Handler):
         pass
     
     def on_msg(self, msg):
-		global players
-		global player
-		print type(players)
-		if 'join' in msg:
-			print str(msg['join']) + ' joined'
-			player = Player.Player(random.randint(0,100), random.randint(0,100), msg['join'], players)
+        global players
+        global borders
 
-
+        if 'join' in msg:
+            print 'player number ' + str(msg['join']) + ' joined'
+            create_player(msg)
+        if 'added_player' in msg:
+            players.append(msg['added_player'])
+        if 'move' in msg:
+            instructions = msg['move']
+            player_number = msg['player_number']
+            player = find_player(player_number)
+                #can return None
+            player.move(instructions[0], instructions[1], borders, players)
         
 host, port = 'localhost', 8888
 client = Client(host, port)
@@ -35,7 +42,38 @@ def periodic_poll():
         poll()
         sleep(0.05)  # seconds
   
+def create_player(self, msg):
+    global players
+    #might not work
+    player = Player.Player(random.randint(0,640), random.randint(0,440), msg['join'], players)
 
+    while hit_player(player):
+        player = Player.Player(random.randint(0,640), random.randint(0,440), msg['join'], players)
+
+    client.do_send({'added_player' : player})
+
+def hit_player(self, player):
+    global border
+    global players
+
+    for border in borders:
+        if player.rect.colliderect(border):
+            return True
+
+    for current_player in players:
+        if player.rect.colliderect(current_player):
+            return True
+
+    return False
+
+def find_player(self, player_number):
+    global players
+
+    for current_player in players:
+        if current_player.get_player_number() == player_number:
+            return current_player
+
+    return None
 
 ##############################
 
@@ -95,107 +133,107 @@ clock = pygame.time.Clock()
 controls = {pygame.K_LEFT : (-1,0), pygame.K_RIGHT : (1,0), pygame.K_UP : (0,-1), pygame.K_DOWN : (0,1)} # player controls
 
 while running:
-	try:
-	    key = pygame.key.get_pressed()
-	    for e in pygame.event.get():
-	        if e.type == pygame.QUIT:
-	            running = False
-	        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
-	            running = False
+    try:
+        key = pygame.key.get_pressed()
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                running = False
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                running = False
 
-	    #start screen
-	    if mode == 0:
-	        title = myfont.render("Werewolf Tag", 1, (255,255,255))
-	        instructions = myfont.render("Press SPACE to Start", 1, (255,255,255))
-	        screen.blit(title, (240, 10))
-	        screen.blit(instructions, (210, 210))
+        #start screen
+        if mode == 0:
+            title = myfont.render("Werewolf Tag", 1, (255,255,255))
+            instructions = myfont.render("Press SPACE to Start", 1, (255,255,255))
+            screen.blit(title, (240, 10))
+            screen.blit(instructions, (210, 210))
 
-	        if key[pygame.K_SPACE]:
-	            mode = 1
-	            now = time.time()
-	            sleep(1)
-	            choose_it(players)
+            if key[pygame.K_SPACE]:
+                mode = 1
+                now = time.time()
+                sleep(1)
+                choose_it(players)
 
-	    #actual game
-	    if mode == 1:
-	        time_up = now + 60
-	        if time.time() >= time_up:
-	            mode = 2
+        #actual game
+        if mode == 1:
+            time_up = now + 60
+            if time.time() >= time_up:
+                mode = 2
 
-	        clock.tick(FRAMERATE)
-	        
-	        # Move the player if an arrow key is pressed
-	        
-	        for pressed in controls:
-	            if key[pressed]:
-	                instruction = controls.get(pressed)
-	                moving_player = player
+            clock.tick(FRAMERATE)
+            
+            # Move the player if an arrow key is pressed
+            
+            for pressed in controls:
+                if key[pressed]:
+                    instruction = controls.get(pressed)
+                    moving_player = player
 
-	                # used to know what instruction players are currently going to see if they can be pushed
-	                # instruction[2].current_dir = (instruction[0], instruction[1])
+                    # used to know what instruction players are currently going to see if they can be pushed
+                    # instruction[2].current_dir = (instruction[0], instruction[1])
 
-	                #if the player has an attribute check if they can still move
-	                if len(moving_player.attributes) > 0:
-	                    for attribute in moving_player.attributes:
-	                        if attribute != "transforming":
-	                            moving_player.move(instruction[0], instruction[1], borders, players)
-	                else:
-						moving_player.move(instruction[0], instruction[1], borders, players)
-						client.do_send({'move': instruction, 'player': player.get_player_number()})
-							
+                    #if the player has an attribute check if they can still move
+                    if len(moving_player.attributes) > 0:
+                        for attribute in moving_player.attributes:
+                            if attribute != "transforming":
+                                moving_player.move(instruction[0], instruction[1], borders, players)
+                    else:
+                        moving_player.move(instruction[0], instruction[1], borders, players)
+                        client.do_send({'move': instruction, 'player': player.get_player_number()})
+                            
 
-	        # Draw the scene
-	        screen.fill((0, 0, 0))
+            # Draw the scene
+            screen.fill((0, 0, 0))
 
 
-	        #check to see if any player is still transforming
-	        for player in players:
-	            player.increase_score(1)
-	            for attribute in player.attributes:
-	                if attribute == "transforming":
-	                    if time.time() >= player.transform_complete:
-	                        player.finish_transform()
+            #check to see if any player is still transforming
+            for player in players:
+                player.increase_score(1)
+                for attribute in player.attributes:
+                    if attribute == "transforming":
+                        if time.time() >= player.transform_complete:
+                            player.finish_transform()
 
-	                    #randomly tested modulo numbers were used for the animation
-	                    elif player.transform_counter %18 == 1:
-	                        player.color = (255, 255, 255)
-	                    elif player.transform_counter %6 == 1:
-	                        player.color = (255, 0, 0)
+                        #randomly tested modulo numbers were used for the animation
+                        elif player.transform_counter %18 == 1:
+                            player.color = (255, 255, 255)
+                        elif player.transform_counter %6 == 1:
+                            player.color = (255, 0, 0)
 
-	                    #counter used to determine which transformation animation should be shown
-	                    player.transform_counter += 1
+                        #counter used to determine which transformation animation should be shown
+                        player.transform_counter += 1
 
-	            player.draw_player(screen)
-	       
-	        disclaimertext = myfont.render("Player 1 score: {0}".format(players[0].get_score()) , 1, (255,255,255))
-	        disclaimertext3 = myfont.render("Time left: {0}".format(round(time_up-time.time(), 2)) , 1, (255,255,255))
-	        screen.blit(disclaimertext, (16, 400))
-	        screen.blit(disclaimertext3, (200, 10))
+                player.draw_player(screen)
+           
+            disclaimertext = myfont.render("Player 1 score: {0}".format(players[0].get_score()) , 1, (255,255,255))
+            disclaimertext3 = myfont.render("Time left: {0}".format(round(time_up-time.time(), 2)) , 1, (255,255,255))
+            screen.blit(disclaimertext, (16, 400))
+            screen.blit(disclaimertext3, (200, 10))
 
-	    #once the time is up!
-	    if mode == 2:
-	        backgroundColor = (0,0,0)
-	        screen.fill(backgroundColor)
+        #once the time is up!
+        if mode == 2:
+            backgroundColor = (0,0,0)
+            screen.fill(backgroundColor)
 
-	        time_up = myfont.render("Time's Up!", 1, (255,255,255))
-	        winner = select_winner(players)
-	        winner_text = myfont.render("The winner is: Player " + str(winner.get_player_number()), 1, (255,255,255))
-	        restart_text = myfont.render("Press Space to continue", 1, (255,255,255))
+            time_up = myfont.render("Time's Up!", 1, (255,255,255))
+            winner = select_winner(players)
+            winner_text = myfont.render("The winner is: Player " + str(winner.get_player_number()), 1, (255,255,255))
+            restart_text = myfont.render("Press Space to continue", 1, (255,255,255))
 
-	        screen.blit(time_up, (240, 10))
-	        screen.blit(winner_text, (210, 210))
-	        screen.blit(restart_text, (210, 220))
+            screen.blit(time_up, (240, 10))
+            screen.blit(winner_text, (210, 210))
+            screen.blit(restart_text, (210, 220))
 
-	        if key[pygame.K_SPACE]:
-	            players = []
-	            mode = 0
+            if key[pygame.K_SPACE]:
+                players = []
+                mode = 0
 
-	    pygame.display.flip()
-	    
+        pygame.display.flip()
+        
 
-	except KeyboardInterrupt:
-		client.do_send({'close' : 'close'})
-		client.do_close()
-		exit()
+    except KeyboardInterrupt:
+        client.do_send({'close' : 'close'})
+        client.do_close()
+        exit()
    # mytxt = sys.stdin.readline().rstrip()
    # client.do_send({'speak': myname, 'txt': mytxt})
--- server.py
+++ server.py
@@ -4,21 +4,28 @@ from time import sleep
 
 handlers = {}  # map client handler to user name
 
+def distribute_msg(msg):
+	for handler in handlers:
+		handler.do_send(msg)
+
 class MyHandler(Handler):
     
     def on_open(self):
     	player_number = len(handlers) + 1
     	handlers[self] = player_number
-        self.do_send({'join': player_number})
-        print player_number
+    	self.do_send({'join': player_number})
+    	print player_number
         
     def on_close(self):
         del(handlers[self])
     
     def on_msg(self, msg):
-		 if 'move' in msg:
-			print(msg['move'])
-			self.do_send(msg)
+                if 'move' in msg:
+                        print(msg['move'])
+                        distribute_msg(msg)
+                if 'added_player' in msg:
+                        distribute_msg(msg)
+
 
 class Serv(Listener):
     handlerClass = MyHandler
