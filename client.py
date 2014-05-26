2428528c1845919dcd2adbddc9fda6ba7aec4ba7
jeffs5 <jeffs5s@gmail.com>
2014-05-26 00:34:32 -0700

    Solid ground for gameplay. Cannot move diagonally

--- client.py
+++ client.py
@@ -16,15 +16,15 @@ class Model():
     def __init__(self):
         self.players = []
         self.borders = [pygame.Rect(0,0, 640, 1), pygame.Rect(0,0, 1, 440), pygame.Rect(639,0, 1, 440), pygame.Rect(0,439, 640, 1)]
-        self.player_numer = None
+        self.player_number = 0
         self.GAME_LENGTH = 30
-        mode = 0
-        loading = False
-        ready = False
-        running = True
-        FRAMERATE = 40
-        clock = pygame.time.Clock()
-        now = time.time()
+        self.mode = 0
+        self.loading = False
+        self.ready = False
+        self.running = True
+        self.FRAMERATE = 60
+        self.clock = pygame.time.Clock()
+        self.now = time.time()
 
     def init_game(self, player_msg):
 
@@ -55,118 +55,46 @@ class View():
 
         # Set up the display
         pygame.display.set_caption("Play Tag!")
-        screen = pygame.display.set_mode((640, 440))
-        myfont = pygame.font.SysFont("monospace", 16)
+        self.screen = pygame.display.set_mode((640, 440))
+        self.myfont = pygame.font.SysFont("monospace", 16)
 
     def display(self):
-        pygame.display.flip()
 
-    ########## Print Screens ###################
-
-    def print_title():
-        title = myfont.render("Werewolf Tag", 1, (255,255,255))
-        intro_message = myfont.render("You are player {0}".format(player_number + 1), 1, (255,255,255))
-        instructions = myfont.render("Press SPACE to Start Countdown", 1, (255,255,255))
-        screen.blit(title, (240, 10))
-        screen.blit(intro_message, (210, 190))
-        screen.blit(instructions, (160, 210))
-
-    def print_game_stats():
-        player_score = myfont.render(
-            "Player: {0}, score: {1}".format(display_number, players[player_number].get_score()) , 1, (255,255,255))
-        time_left = myfont.render("Time left: {0}".format(int(time_up-time.time()) + 1) , 1, (255,255,255))
-        screen.blit(player_score, (16, 400))
-        screen.blit(time_left, (220, 10))
-
-    def print_end_game():
-        time_up = myfont.render("Time's Up!", 1, (255,255,255))
-        winner = select_winner(players)
-        winner_number = winner.get_player_number() + 1
-        winner_text = myfont.render(
-            "The winner is: Player {0} with a score of {1}".format(winner_number, winner.get_score()), 1, (255,255,255))
-        your_text = myfont.render("Your score was: {0} ".format(players[player_number].get_score()), 1, (255,255,255))
-        restart_text = myfont.render("Press Space to continue", 1, (255,255,255))
-
-        screen.blit(time_up, (240, 10))
-        screen.blit(winner_text, (110, 210))
-        screen.blit(your_text, (200, 225))
-        screen.blit(restart_text, (180, 235))
-
-
-##################  CONTROLLER #######################
-
-class Controller():
-    def __init__(self, model):
-        self.m = model
-        self.controls = {pygame.K_LEFT : (-1,0), pygame.K_RIGHT : (1,0), pygame.K_UP : (0,-1), pygame.K_DOWN : (0,1)} # player controls
-
-        
-    def get_commands(self):
-        key = pygame.key.get_pressed()
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                self.m.running = False
-            if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
-                self.m.running = False
-
-         #start screen
+                 #start screen
         if self.m.mode == 0:
-            screen.fill((0, 0, 0))
-            print_title()
-
-            if key[pygame.K_SPACE]:
-                if not self.m.loading:
-                    self.m.loading = True
-                    client.do_send("load")
+            self.screen.fill((0, 0, 0))
+            self.print_title()
 
         #countdown screen       
-        if self.m.mode == 1:
+        elif self.m.mode == 1:
             # clear screen
-            screen.fill((0, 0, 0))
+            self.screen.fill((0, 0, 0))
 
             time_up = self.m.now + 5
             if time.time() < time_up:
-                instructions = myfont.render("Get Ready to start!", 1, (255,255,255))
-                countdown = myfont.render("{0}".format(int(time_up-time.time()) + 1) , 1, (255,255,255))
-                screen.blit(instructions, (210, 210))
-                screen.blit(countdown, (300, 230))
-            elif self.m.ready == False:
-                self.m.ready = True
-                return "ready"
+                instructions = self.myfont.render("Get Ready to start!", 1, (255,255,255))
+                countdown = self.myfont.render("{0}".format(int(time_up-time.time()) + 1) , 1, (255,255,255))
+                self.screen.blit(instructions, (210, 210))
+                self.screen.blit(countdown, (300, 230))
 
         #actual game
-        if self.m.mode == 2:
+        elif self.m.mode == 2:
             ## change for time
-            time_up = now + self.m.GAME_LENGTH
+            time_up = self.m.now + self.m.GAME_LENGTH
             if time.time() >= time_up:
                 self.m.mode = 3
 
             else:
-                clock.tick(self.m.FRAMERATE)
-
-                # Move the player if an arrow key is pressed
-
-                for pressed in self.controls:
-                    if key[pressed]:
-                        instruction = self.controls.get(pressed)
-                        moving_player = self.m.players[self.m.player_number]
-
-                        #if the player has an attribute check if they can still move
-                        if len(moving_player.attributes) > 0:
-                            for attribute in moving_player.attributes:
-                                if attribute != "transforming":
-                                   return {'move': self.m.instruction, 'player': self.m.player_number}
-                        else:
-                            return {'move': self.m.instruction, 'player': self.m.player_number}
+                # take out?
+                # clock.tick(self.m.FRAMERATE)
 
 
                 # Draw the scene
-                screen.fill((0, 0, 0))
+                self.screen.fill((0, 0, 0))
 
 
                 #check to see if any player is still transforming
                 for player in self.m.players:
-                    player.increase_score(1)
                     for attribute in player.attributes:
                         if attribute == "transforming":
                             if time.time() >= player.transform_complete:
@@ -181,38 +109,151 @@ class Controller():
                             #counter used to determine which transformation animation should be shown
                             player.transform_counter += 1
 
-                    player.draw_player(screen)
-                display_number = player_number + 1
-                print_game_stats()
+                    player.draw_player(self.screen)
+                display_number = self.m.player_number + 1
+                self.print_game_stats(time_up)
 
         #once the time is up!
         if self.m.mode == 3:
             backgroundColor = (0,0,0)
             screen.fill(backgroundColor)
-            print_end_game()
+            self.print_end_game()
+
+        pygame.display.flip()
+
+    ########## Print Screens ###################
+
+    def print_title(self):
+        title = self.myfont.render("Werewolf Tag", 1, (255,255,255))
+        intro_message = self.myfont.render("You are player {0}".format(self.m.player_number + 1), 1, (255,255,255))
+        instructions = self.myfont.render("Press SPACE to Start Countdown", 1, (255,255,255))
+        self.screen.blit(title, (240, 10))
+        self.screen.blit(intro_message, (210, 190))
+        self.screen.blit(instructions, (160, 210))
+
+    def print_game_stats(self, time_up):
+        display_number = self.m.player_number + 1
+
+        player_score = self.myfont.render(
+            "Player: {0}, score: {1}".format(display_number, self.m.players[self.m.player_number].get_score()), 1, (255,255,255))
+        time_left = self.myfont.render("Time left: {0}".format(time_up-time.time() + 1), 1, (255,255,255))
+        self.screen.blit(player_score, (16, 400))
+        self.screen.blit(time_left, (220, 10))
+
+    def print_end_game(self):
+        time_up = self.myfont.render("Time's Up!", 1, (255,255,255))
+        winner = select_winner(self.m.players)
+        winner_number = winner.get_player_number() + 1
+        winner_text = self.myfont.render(
+            "The winner is: Player {0} with a score of {1}".format(winner_number, winner.get_score()), 1, (255,255,255))
+        your_text = self.myfont.render("Your score was: {0} ".format(self.m.players[self.m.player_number].get_score()), 1, (255,255,255))
+        restart_text = self.myfont.render("Press Space to continue", 1, (255,255,255))
+
+        self.screen.blit(time_up, (240, 10))
+        self.screen.blit(winner_text, (110, 210))
+        self.screen.blit(your_text, (200, 225))
+        self.screen.blit(restart_text, (180, 235))
+
+
+##################  CONTROLLER #######################
+
+class Controller():
+    def __init__(self, model):
+        self.m = model
+        self.controls = {pygame.K_LEFT : (-1,0), pygame.K_RIGHT : (1,0), pygame.K_UP : (0,-1), pygame.K_DOWN : (0,1)} # player controls
+
+        
+    def get_commands(self):
+
+        key = pygame.key.get_pressed()
+
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                self.m.running = False
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                self.m.running = False
+
+         #start screen
+        if self.m.mode == 0:
+
+            if key[pygame.K_SPACE]:
+                if not self.m.loading:
+                    self.m.loading = True
+                    return "load"
+
+        #countdown screen       
+        elif self.m.mode == 1:
+
+            self.m.time_up = self.m.now + 5
+
+            if not self.m.ready:
+                self.m.ready = True
+                return "ready"
+
+        #actual game
+        elif self.m.mode == 2:
+            ## change for time
+            time_up = self.m.now + self.m.GAME_LENGTH
+            if time.time() >= time_up:
+                self.m.mode = 3
+
+            else:
+                self.m.clock.tick(self.m.FRAMERATE)
+
+                # Move the player if an arrow key is pressed
+
+                for pressed in self.controls:
+                    if key[pressed]:
+                        instruction = self.controls.get(pressed)
+                        moving_player = self.m.players[self.m.player_number]
+
+                        #if the player has an attribute check if they can still move
+                        if len(moving_player.attributes) > 0:
+                            for attribute in moving_player.attributes:
+                                if attribute != "transforming":
+                                   return {'move': instruction, 'player': self.m.player_number}
+                        else:
+                            return {'move': instruction, 'player': self.m.player_number}
+
+                #check to see if any player is still transforming
+                for player in self.m.players:
+                    player.increase_score(1)
+                    for attribute in player.attributes:
+                        if attribute == "transforming":
+                            if time.time() >= player.transform_complete:
+                                player.finish_transform()
+
+                            #counter used to determine which transformation animation should be shown
+                            player.transform_counter += 1
+
+        #once the time is up!
+        elif self.m.mode == 3:
 
             if key[pygame.K_SPACE]:
                 self.m.mode = 0
 
+        return "no message"
 
-    def move_player(player, instruction):
-        player.move(instruction[0], instruction[1], borders, players)
+
+    def move_player(self, player, instruction):
+        player.move(instruction[0], instruction[1], self.m.borders, self.m.players)
 
     def select_winner(players):
-    winner = players[0]
-    for player in players:
-        if player.get_score() > winner.get_score():
-            winner = player
+        winner = self.m.players[0]
+        for player in self.m.players:
+            if player.get_score() > winner.get_score():
+                winner = player
 
-    return winner
+        return winner
 
 ##################  NETWORK CONTROLLER ###############
 
 class NetworkController(Handler): 
 
-    def __init__(self, model, host, port):
+    def __init__(self, controller, model, host, port):
         Handler.__init__(self, host, port)
         self.m = model
+        self.c = controller
 
     def on_close(self):
         pass ## no need for it to do anything
@@ -235,39 +276,39 @@ class NetworkController(Handler):
 
         elif 'move' in msg:
             moved_player = msg['player']
-            player = players[moved_player]
-            move_player(player, msg['move'])
+            player = self.m.players[moved_player]
+            self.c.move_player(player, msg['move'])
 
             #used for error handling
         else:
             print msg
 
-    def send_message(self,message):
-        client.do_send(message)
+    def send_message(self, message):
+        self.do_send(message)
 
     def poll_messages(self):
-        poll(.05)
+        poll()
+        sleep(.05)
 
 
 #################  MAIN LOOP  ####################
 
 host, port = 'localhost', 8888
-client = Client(host, port)
 
 m = Model()
 v = View(m)
-c = Controller(c)
-n = NetworkController(m, host, port)
+c = Controller(m)
+n = NetworkController(c, m, host, port)
 
-while running:
+while m.running:
     try:
         # Network Controller 
         n.poll_messages()
-        message = c.get_commands
+        message = c.get_commands()
         n.send_message(message)
         v.display()
 
     except KeyboardInterrupt:
-        client.do_send({'close' : 'close'})
-        client.do_close()
+        n.do_send({'close' : 'close'})
+        n.do_close()
         exit()
