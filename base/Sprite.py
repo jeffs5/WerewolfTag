65c0844c19092698ab797e0d56a11049252dda00
Angel Hernandez <angelh5@uci.edu>
2014-12-16 00:27:59 -0800

    Initial werewolf animation

--- base/Sprite.py
+++ base/Sprite.py
@@ -13,9 +13,19 @@ class Sprite:
     def __init__(self):
         self.offsetX = 0
         self.offsetY = 0
-        self.rect = pygame.Rect(0, 0, 0, 0) 
         self.surface = None
-        
+        self.rect = pygame.Rect(0, 0, 0, 0) 
+       
+    # 
+    # update frame helper method
+    #
+    def updateFrame(self, frame):
+        # update sprite frame
+        self.offsetX = frame[0]
+        self.offsetY = frame[1]
+        self.rect.width = frame[2]
+        self.rect.height = frame[3] 
+    
     #
     # draw sprite
     #
@@ -25,12 +35,3 @@ class Sprite:
             # draw surface to screen
             screen.blit(self.surface, (self.rect.x, self.rect.y), pygame.Rect(self.offsetX, self.offsetY, self.rect.width, self.rect.height))
             
-    # 
-    # update frame helper method
-    #
-    def updateFrame(self, frame):
-        # update sprite frame
-        self.offsetX = frame[0]
-        self.offsetY = frame[1]
-        self.rect.width = frame[2]
-        self.rect.height = frame[3]
